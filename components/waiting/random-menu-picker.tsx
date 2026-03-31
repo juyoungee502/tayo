c5b43002af52ff79b0bd5dd30199eb2963a048ab
쭈영 <submel@naver.com>
2026-03-31 11:08:04 +0900

    expand random menu options

--- components/waiting/random-menu-picker.tsx
+++ components/waiting/random-menu-picker.tsx
@@ -1,11 +1,30 @@
-﻿"use client";
+"use client";
 
 import { useState } from "react";
 
-import { shareRandomMenuAction } from "@/lib/actions/app-actions";
 import { buttonStyles } from "@/components/ui/button";
+import { shareRandomMenuAction } from "@/lib/actions/app-actions";
 
 const MENU_CANDIDATES = [
+  "유부초밥 + 떡볶이",
+  "불닭볶음면",
+  "카레돈가스",
+  "콩나물불고기",
+  "라멘",
+  "초밥",
+  "회덮밥",
+  "간장새우덮밥",
+  "파스타",
+  "피자",
+  "치킨",
+  "한솥",
+  "지지고",
+  "학식",
+  "엽기떡볶이",
+  "김치찜",
+  "꼬밥",
+  "나비루",
+  "이삭토스트",
   "마라탕 + 꿔바로우",
   "떡볶이 + 튀김",
   "김치볶음밥 + 계란후라이",
@@ -14,10 +33,27 @@ const MENU_CANDIDATES = [
   "햄버거 세트",
   "국밥",
   "쌀국수",
-  "파스타",
   "치킨마요",
   "불닭 + 삼각김밥",
   "순두부찌개",
+  "부대찌개",
+  "제육덮밥",
+  "알밥",
+  "냉면 + 만두",
+  "로제떡볶이",
+  "우동 + 유부초밥",
+  "샤브샤브",
+  "타코야끼 + 어묵탕",
+  "삼겹살 정식",
+  "규동",
+  "텐동",
+  "비빔밥",
+  "참치김밥 + 라면",
+  "스테이크 덮밥",
+  "토마토 파스타 + 마늘빵",
+  "치즈돈가스",
+  "닭갈비 볶음밥",
+  "로코모코",
 ];
 
 function pickRandomMenu() {
